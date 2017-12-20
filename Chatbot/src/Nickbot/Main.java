f3bddaf5f5df3720e0c54b079f817055b67b39b6
Rendall Schijven <rendall.schijven@hotmail.com>
2017-12-20 13:30:12 +0100

    Added the setup for our chatbot

---
+++ Chatbot/src/Nickbot/Main.java
@@ -0,0 +1,31 @@
+package Nickbot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+/**
+ *
+ * @author Groep 16
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("CallToPrintStackTrace")
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new Nickbot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
