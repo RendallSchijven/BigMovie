371631c687692615e05d99673daa63b471537041
twan terschegget <twanterschegget@hotmail.nl>
2017-12-22 09:55:00 +0100

    Started on teh mpaaParser

--- Parser/src/nl/nhl/groep16/parser/parsers/MovieParser.java
+++ Parser/src/nl/nhl/groep16/parser/parsers/MovieParser.java
@@ -1,8 +1,32 @@
 package nl.nhl.groep16.parser.parsers;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MovieParser extends AbstractParser {
+
+    private final Pattern movieRegex;
+
+    public MovieParser() {
+        this.movieRegex = Pattern.compile("^[^\"](.*?)\\t");
+    }
+
     @Override
     public String[] convertLine(String line) {
-        return new String[0];
+
+        String movie = extractMovie(line);
+        if (movie == null) {
+            return null;
+        }
+        return new String[]{movie};
+    }
+
+    public String extractMovie(String line) {
+        Matcher m = movieRegex.matcher(line);
+        if (!m.find()) {
+            return null;
+        }
+
+        return m.group(0).replaceAll("\t", "");
     }
 }
