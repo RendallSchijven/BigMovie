d55daded121cd6c5724da1e97e70248808fef0f4
twan terschegget <twanterschegget@hotmail.nl>
2018-01-03 12:55:43 +0100

    The mpaaParser fix after git problems and rename

--- Parser/src/nl/nhl/groep16/parser/parsers/MpaaParser.java
+++ Parser/src/nl/nhl/groep16/parser/parsers/MpaaParser.java
@@ -3,7 +3,7 @@ package nl.nhl.groep16.parser.parsers;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class mpaaParser extends AbstractParser {
+public class MpaaParser extends AbstractParser {
 
     private final Pattern ratingRegex;
     private final Pattern moviesRegex;
@@ -11,7 +11,7 @@ public class mpaaParser extends AbstractParser {
     private String currentMovie = "";
     private String currentRating = "";
 
-    public mpaaParser() {
+    public MpaaParser() {
         this.ratingRegex = Pattern.compile("RE:\\sRated\\s([^\\s]+)");
         this.moviesRegex = Pattern.compile("MV:(.[^\"].*)");
     }
