dcd4277d345e6974bc7256c3dde2e9420898098f
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-08 11:47:04 +0100

    Modified R script to produce graph of actors

--- Chatbot/resources/R/video-format.R
+++ Chatbot/resources/R/video-format.R
@@ -1,12 +1,17 @@
 #!/usr/bin/Rscript
 
-# install.packages("RMySQL")
+install.packages("RMySQL")
 library(RMySQL)
 
-con <- dbConnect(MySQL(), dbname="imdb", user="imdb", password="imdb")
-values <- dbGetQuery(con, "select movie_info.info as format, count(*) as freq from movie_info, info_type where info_type.info = 'LD picture format' and info_type_id = info_type.id group by movie_info.info")
-
-invisible(jpeg('/tmp/video-format.jpg'))
-barplot(values$freq, names.arg = values$format, horiz=FALSE, cex.names=0.5)
-invisible(dev.off())
+if(mysqlHasDefault()){
+  mydb <- dbConnect(MySQL(), dbname="NickyBot", user="Riley", password="jayden", host="hiddevanranden.nl")
+  
+  values <- dbGetQuery(mydb, "select Name as names, count(*) as freq from Persons, Persons_Movies where Persons.ID = Persons_Movies.Person_ID and Role_ID = 1 group by Persons.Name")
+  
+  invisible(jpeg('rplot.jpg'))
+  barplot(values$freq, names.arg = values$names, horiz=FALSE, cex.names=0.5)
+  invisible(dev.off())
+  
+  dbDisconnect(mydb)
+}
 
