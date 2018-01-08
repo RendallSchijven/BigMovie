328cff9b5fb0126e56697471af83b08c6840baed
twan terschegget <twanterschegget@hotmail.nl>
2018-01-08 12:04:33 +0100

    Release Parser added

--- Parser/src/nl/nhl/groep16/parser/Main.java
+++ Parser/src/nl/nhl/groep16/parser/Main.java
@@ -25,9 +25,9 @@ public class Main {
 //        }
 
 //        BufferedReader br = new BufferedReader(new FileReader("D:/IMDB/mpaa-ratings-reasons.list"));
-        BufferedReader br = new BufferedReader(new FileReader("C:/Users/twant/Desktop/ratings.list"));
+        BufferedReader br = new BufferedReader(new FileReader("C:/Users/twant/Desktop/Data/release-dates.list"));
 
-        ParserInterface parser = new RatingsParser();
+        ParserInterface parser = new ReleaseParser();
         String line;
         while ((line = br.readLine()) != null) {
             String[] out = parser.convertLine(line);
