7d1c71cee87d6242f09174e6be77ec84eb94d61a
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-15 10:56:13 +0100

    Added 3 R questions and scripts

---
+++ Chatbot/resources/R/R1.R
@@ -0,0 +1,5 @@
+# Title     : MPAA Motivation
+# Objective : Bepaal de genre van een film op basis van motivatie van MPAA
+# Created by: Rendall
+# Created on: 15-1-2018
+
---
+++ Chatbot/resources/R/R2.R
@@ -0,0 +1,5 @@
+# Title     : MPAA Plot
+# Objective : Leidt de MPAA af van het plot van een film
+# Created by: Rendall
+# Created on: 15-1-2018
+
