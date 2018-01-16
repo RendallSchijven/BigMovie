228c2b54a5e8c4a218b27a0801da39e3570cd081
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-16 11:56:57 +0100

    Updated query for visualization 2

--- Chatbot/resources/R/Visualization1.R
+++ Chatbot/resources/R/Visualization1.R
@@ -1,5 +1,5 @@
-# Title     : TODO
-# Objective : TODO
+# Title     : Films per land van herkomst
+# Objective : Maak een kaart (b.v. google maps / openstreetview) met landen waar een flim speelt. Zodat op de kaart te zien is waar de films spelen.
 # Created by: Rendall
 # Created on: 15-1-2018
 
