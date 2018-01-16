0a03312615d6865ac6e0049f578808dc181470f0
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-16 23:23:10 +0100

    Added AGE to db query

--- Chatbot/resources/R/R2.R
+++ Chatbot/resources/R/R2.R
@@ -9,7 +9,15 @@ mydb <- dbConnect(MySQL(), dbname="NickyBot", user="Riley", password="jayden", h
 
 actress <- dbGetQuery(mydb, "select ID, BirthDay, Name, count(*) as freq FROM Persons LEFT JOIN Persons_Movies ON Persons_Movies.Person_ID = Persons.ID WHERE Persons.Sex = 'Female' AND Persons_Movies.Role = 'actor' GROUP BY Persons.ID ORDER BY freq DESC LIMIT 2000")
 moviesPerYearPerActress <- dbGetQuery(mydb, "select Name, ReleaseYear, count(*) as freq FROM Persons, Movies, Persons_Movies WHERE Persons_Movies.Role = 'actor' AND Persons_Movies.Person_ID = Persons.ID GROUP BY Movies.ReleaseYear ORDER BY freq DESC LIMIT 4000")
+careerLength <- dbGetQuery(mydb, "SELECT Name, (MAX(m.ReleaseYear) - MIN(m.ReleaseYear)) AS CareerLength, (CAST('2018-01-16' AS DATE) - CAST(BirthDay AS DATE))/1000 AS AGE FROM Persons AS p
+                                  LEFT JOIN Persons_Movies pm
+                                  ON pm.Person_ID = p.ID AND pm.Role = 'actor'
 
+                                  LEFT JOIN Movies m
+                                  ON pm.Movie_ID = m.ID
+
+                                  GROUP BY p.ID
+                                  ORDER BY CareerLength DESC;")
 
 for(id in 1:2000){
   movies <- dbGetQuery(mydb, paste("select Title from Movies, Persons_Movies where Persons_Movies.Person_ID = (" ,id, ")", sep=""))
