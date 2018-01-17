5155e4c696b9894d037dbab17d4d912e7d827eb9
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-17 11:43:11 +0100

    Finished R question 2

--- Chatbot/resources/R/R2.R
+++ Chatbot/resources/R/R2.R
@@ -1,5 +1,5 @@
-# Title     : Leeftijd actrice
-# Objective : Kijk of een actrice minder werk heeft naarmate ze ouder word
+# Title     : Costly length
+# Objective : Ga na of er een verband is tussen de kosten van een film en de lengte ervan.
 # Created by: Rendall
 # Created on: 15-1-2018
 
@@ -7,20 +7,13 @@ library(RMySQL)
 
 mydb <- dbConnect(MySQL(), dbname="NickyBot", user="Riley", password="jayden", host="hiddevanranden.nl")
 
-actress <- dbGetQuery(mydb, "select ID, BirthDay, Name, count(*) as freq FROM Persons LEFT JOIN Persons_Movies ON Persons_Movies.Person_ID = Persons.ID WHERE Persons.Sex = 'Female' AND Persons_Movies.Role = 'actor' GROUP BY Persons.ID ORDER BY freq DESC LIMIT 2000")
-moviesPerYearPerActress <- dbGetQuery(mydb, "select Name, ReleaseYear, count(*) as freq FROM Persons, Movies, Persons_Movies WHERE Persons_Movies.Role = 'actor' AND Persons_Movies.Person_ID = Persons.ID GROUP BY Movies.ReleaseYear ORDER BY freq DESC LIMIT 4000")
-careerLength <- dbGetQuery(mydb, "SELECT Name, (MAX(m.ReleaseYear) - MIN(m.ReleaseYear)) AS CareerLength, (CAST('2018-01-16' AS DATE) - CAST(BirthDay AS DATE))/10000 AS AGE FROM Persons AS p
-                                  LEFT JOIN Persons_Movies pm
-                                  ON pm.Person_ID = p.ID AND pm.Role = 'actor'
-
-                                  LEFT JOIN Movies m
-                                  ON pm.Movie_ID = m.ID
-
-                                  GROUP BY p.ID
-                                  ORDER BY CareerLength DESC;")
-
-
-movies <- dbGetQuery(mydb, paste("select Title from Movies, Persons_Movies where Persons_Movies.Person_ID = (" ,id, ")", sep=""))
-
+movies <- dbGetQuery(mydb, "SELECT ID, Budget, Duration FROM Movies WHERE Duration IS NOT NULL AND Budget IS NOT NULL")
 
 dbDisconnect(mydb)
+
+plot(movies)
+cor(movies)
+
+#Maak een lineair regressie model van movies
+movielm = lm(Budget ~ Duration, data=movies)
+summary(movielm)
