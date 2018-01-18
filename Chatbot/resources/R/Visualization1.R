66b9f5558842a29b40e4bdc846b43b5f1644dd59
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-18 22:09:00 +0100

    Added 2 maybe faster queries for question 3

--- Chatbot/resources/R/Visualization1.R
+++ Chatbot/resources/R/Visualization1.R
@@ -1,5 +1,5 @@
 # Title     : Visual genre
-# Objective : Maak een visuele presentatie van de in de loop van de tijd veranderende populariteit vanonderwerpen (genre) van films.
+# Objective : Maak een visuele presentatie van de in de loop van de tijd veranderende populariteit van onderwerpen (genre) van films.
 # Created by: Rendall
 # Created on: 15-1-2018
 
