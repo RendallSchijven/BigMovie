493d1422215cefa4c36b0f625cee631c178cd386
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-22 11:48:29 +0100

    Changed database connection

--- Chatbot/src/Nickybot/JdbcSubroutine.java
+++ Chatbot/src/Nickybot/JdbcSubroutine.java
@@ -1,12 +1,7 @@
 package Nickybot;
 
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Statement;
 import com.rivescript.macro.Subroutine;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 /**
  *
@@ -16,18 +11,16 @@ public class JdbcSubroutine implements Subroutine {
 
     @Override
     public String call(com.rivescript.RiveScript rs, String[] args) {
-        String host = "db.sanderkastelein.nl";
-        String port = "3306";
-        String db = "NickyBot";
-        String username = "riley";
-        String password = "jayden";
+
         String sql = "";
         String result = "";
+
         for (String arg : args) sql = sql + " " + arg;
         sql = sql.trim();
         System.out.println(sql);
+        result = Database.query(sql);
 
-        return Database.query(sql);
+        return result;
     }
 
 }
