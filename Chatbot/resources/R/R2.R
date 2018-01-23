42c1fc0d3f4421f3bcf1f6c0c2759992bd83f3af
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-23 12:58:16 +0100

    MPAA now always predicts R

--- Chatbot/resources/R/R2.R
+++ Chatbot/resources/R/R2.R
@@ -7,16 +7,18 @@ library(RMySQL)
 
 mydb <- dbConnect(MySQL(), dbname="NickyBotUtf8", user="riley", password="jayden", host="db.sanderkastelein.nl")
 
-movies <- dbGetQuery(mydb, "SELECT ID, Budget, Duration FROM Movies WHERE Duration IS NOT NULL AND Budget IS NOT NULL")
+query <- ("SELECT Budget, Rating, Duration FROM Movies WHERE Duration IS NOT NULL AND Budget IS NOT NULL AND Currency = 'USD' AND Rating IS NOT NULL")
+  
+movies <- dbGetQuery(mydb, query)
 
-dbDisconnect(mydb)
+invisible(dbDisconnect(mydb))
 
-plot(movies$Budget, movies$Duration, main="Correlatie tussen de kosten en de lengte van een film", xlab="Budget van een film", ylab="Lengte van een film")
+plot(movies$Budget / 10000, movies$Duration, main="Correlatie tussen de kosten en de lengte van een film", xlab="Budget van een film in $ x10000", ylab="Duur van een film")
+scatter.smooth(movies$Budget, movies$Duration, main="Budget ~ Duration")
 cor(movies)
 
 #Maak een lineair regressie model van movies
-movielm = lm(Budget ~ Duration, data=movies)
+movielm = lm(Rating ~ Budget, data=movies)
 summary(movielm)
 
-movieglm = glm(Budget ~ Duration, data=movies, family=binomial)
-summary(movieglm)
+plot(movies)
