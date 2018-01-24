00162996346fbf0c093f2fc290540903e60f3fbd
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-24 15:04:26 +0100

    Made rivescript multiline, R1 works

--- Chatbot/resources/R/MPAA.R
+++ Chatbot/resources/R/MPAA.R
@@ -16,14 +16,24 @@ library(caTools)
 library(rpart)
 library(rpart.plot)
 
-mydb <- dbConnect(MySQL(), dbname="NickyBotUtf8", user="riley", password="jayden", host="db.sanderkastelein.nl")
+mydb <- dbConnect(MySQL(), dbname="NickyBotUtf82", user="riley", password="jayden", host="db.sanderkastelein.nl")
 
-query <- ("SELECT MPAA, Plot FROM Movies WHERE MPAA IS NOT NULL AND Plot IS NOT NULL")
+getR <- ("SELECT MPAA, Plot FROM Movies WHERE MPAA = 'R' AND Plot IS NOT NULL ORDER BY RAND() LIMIT 1783")
+getPG <- ("SELECT MPAA, Plot FROM Movies WHERE MPAA = 'PG' AND Plot IS NOT NULL ORDER BY RAND() LIMIT 1783")
+getPG13 <- ("SELECT MPAA, Plot FROM Movies WHERE MPAA = 'PG-13' AND Plot IS NOT NULL ORDER BY RAND() LIMIT 1783")
 
-movies <- dbGetQuery(mydb, query)
+testMovie <- dbGetQuery(mydb, ("SELECT Plot, MPAA FROM Movies WHERE Title = 'Zodiac (2007)'"))
+testMovie$MPAA <- 'z'
+
+moviesR <- dbGetQuery(mydb, getR)
+moviesPG <- dbGetQuery(mydb, getPG)
+moviesPG13 <- dbGetQuery(mydb, getPG13)
+
+movies = rbind(moviesR, moviesPG, moviesPG13, testMovie)
 
 invisible(dbDisconnect(mydb))
 
+#Prepare all plots for testing
 corpus = Corpus(VectorSource(movies$Plot))
 corpus = tm_map(corpus, tolower)
 corpus = tm_map(corpus, removePunctuation)
@@ -31,7 +41,7 @@ corpus = tm_map(corpus, removeWords, c(stopwords("english")))
 corpus = tm_map(corpus, stemDocument)
 
 frequencies = DocumentTermMatrix(corpus)
-sparse = removeSparseTerms(frequencies, 0.995)
+sparse = removeSparseTerms(frequencies, 0.94)
 
 moviesSparse = as.data.frame(as.matrix(sparse))
 
@@ -39,15 +49,18 @@ colnames(moviesSparse) = make.names(colnames(moviesSparse))
 
 moviesSparse$MPAA = movies$MPAA
 
-set.seed(123)
+#set.seed(123)
 split = sample.split(moviesSparse$MPAA, SplitRatio = 0.7)
-train = subset(moviesSparse, split == TRUE)
-test = subset(moviesSparse, split == FALSE)
+train = subset(moviesSparse, MPAA != 'z')
+test = subset(moviesSparse, MPAA = 'z')
 
-movieCart = rpart(MPAA ~ ., data=train, method = "class")
+movieCart = rpart(MPAA ~ ., data=moviesSparse, method = "class")
 prp(movieCart)
 
 predictCart = predict(movieCart, newdata = test, type = "class")
 
 table = table(test$MPAA, predictCart)
 print(table)
+
+accuracy = sum(diag(table))/sum(table)
+print(accuracy)
