26b8a600ef53ce207b54a36cf7d180f1df968be2
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-25 00:02:23 +0100

    RiveScript over mappen verdeeld

--- Chatbot/resources/R/ENRScript/R3en.R
+++ Chatbot/resources/R/ENRScript/R3en.R
@@ -7,12 +7,12 @@ library(RMySQL)
 
 mydb <- dbConnect(MySQL(), dbname="NickyBotUtf82", user="riley", password="jayden", host="db.sanderkastelein.nl")
 
-query <- ("SELECT Budget, Rating FROM Movies WHERE Budget IS NOT NULL AND Currency = 'USD' AND Rating IS NOT NULL")
+query <- ("SELECT Budget, Rating FROM Movies WHERE Budget IS NOT NULL AND Currency = 'USD' AND Rating IS NOT NULL ORDER BY RAND() LIMIT 5000")
 
 movies <- dbGetQuery(mydb, query)
 
 invisible(dbDisconnect(mydb))
 
 invisible(jpeg('BudgetRating.jpg'))
-scatter.smooth(movies$Budget / 10000, movies$Duration, main="Relation between movie budget and ratings", col = "green", lpars =list(col = "red", lwd = 3, lty = 3), xlab = "Budget in $ x10000", ylab = "Rating")
+scatter.smooth(movies$Budget / 10000, movies$Rating, main="Relation between movie budget and ratings", col = "green", lpars =list(col = "red", lwd = 3, lty = 3), xlab = "Budget in $ x10000", ylab = "Rating")
 invisible(dev.off())
