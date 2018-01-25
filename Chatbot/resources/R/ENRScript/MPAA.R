0c9f51a3cefb391ed542d2d11b131f311a239054
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-25 10:57:27 +0100

    Added stop condition to MPAA

--- Chatbot/resources/R/ENRScript/MPAA.R
+++ Chatbot/resources/R/ENRScript/MPAA.R
@@ -16,45 +16,62 @@ library(caTools)
 library(rpart)
 library(rpart.plot)
 
-mydb <- dbConnect(MySQL(), dbname="NickyBotUtf82", user="riley", password="jayden", host="db.sanderkastelein.nl")
+#The connection string for the database
+mydb <- dbConnect(MySQL(), dbname="NickyBotUtf8", user="riley", password="jayden", host="db.sanderkastelein.nl")
 
-getR <- ("SELECT MPAA, Plot FROM Movies WHERE MPAA = 'R' AND Plot IS NOT NULL ORDER BY RAND() LIMIT 1783")
-getPG <- ("SELECT MPAA, Plot FROM Movies WHERE MPAA = 'PG' AND Plot IS NOT NULL ORDER BY RAND() LIMIT 1783")
-getPG13 <- ("SELECT MPAA, Plot FROM Movies WHERE MPAA = 'PG-13' AND Plot IS NOT NULL ORDER BY RAND() LIMIT 1783")
+#The queries for getting the different MPAA we left out NC-17 and G because only 1% of the movies had this MPAA
+#We limited to 1783 so the model would be fairly distributed over all 3 values
+getR <- ("SELECT ID, MPAA, Plot FROM Movies WHERE MPAA = 'R' AND Plot IS NOT NULL ORDER BY RAND() LIMIT 1783")
+getPG <- ("SELECT ID, MPAA, Plot FROM Movies WHERE MPAA = 'PG' AND Plot IS NOT NULL ORDER BY RAND() LIMIT 1783")
+getPG13 <- ("SELECT ID, MPAA, Plot FROM Movies WHERE MPAA = 'PG-13' AND Plot IS NOT NULL ORDER BY RAND() LIMIT 1783")
 
-testMovie <- dbGetQuery(mydb, ("SELECT Plot, MPAA FROM Movies WHERE Title = 'Zodiac (2007)'"))
-testMovie$MPAA <- 'z'
+#Get the movie data from the movie the user entered
+testMovie <- dbGetQuery(mydb, ("SELECT ID, Plot, MPAA FROM Movies WHERE Title = 'Baywatch (2017)'"))
 
+#Make MPAA empty so we can predict it
+testMovie$MPAA <- ''
+
+#Save the ID so we can use it for the split
+testMovieID <- testMovie$ID
+
+#Check if plot is empty if so return this
+if(is.na(testMovie$Plot)){stop("This movie has no plot we can predict of, please choose another movie.")}
+
+#Get the movies for each MPAA
 moviesR <- dbGetQuery(mydb, getR)
 moviesPG <- dbGetQuery(mydb, getPG)
 moviesPG13 <- dbGetQuery(mydb, getPG13)
 
+#Bind the datasets into one
 movies = rbind(moviesR, moviesPG, moviesPG13, testMovie)
 
+#Disconnect from the database
 invisible(dbDisconnect(mydb))
 
-#Prepare all plots for testing
+#Prepare all plots for testing by cleaning them up
 corpus = Corpus(VectorSource(movies$Plot))
 corpus = tm_map(corpus, tolower)
 corpus = tm_map(corpus, removePunctuation)
 corpus = tm_map(corpus, removeWords, c(stopwords("english")))
 corpus = tm_map(corpus, stemDocument)
 
+#This function removes all but the top 6% of words from the dataset
 frequencies = DocumentTermMatrix(corpus)
 sparse = removeSparseTerms(frequencies, 0.94)
 
+#Make a dataset of the sparse words we can use for the cart model.
 moviesSparse = as.data.frame(as.matrix(sparse))
 
+#Make the colnames acceptable
 colnames(moviesSparse) = make.names(colnames(moviesSparse))
 
+#Add the MPAA AND ID to the sparse dataset
 moviesSparse$MPAA = movies$MPAA
+moviesSparse$ID = movies$ID
 
-#set.seed(123)
-split = sample.split(moviesSparse$MPAA, SplitRatio = 0.7)
-
-#WERKT NIET
-train = subset(moviesSparse, MPAA != 'z')
-test = subset(moviesSparse, MPAA = 'z')
+#Split the data into a trainset and testset where the testset is the plot of the movie the user entered
+train = subset(moviesSparse, ID != testMovieID)
+test = subset(moviesSparse, ID == testMovieID)
 
 movieCart = rpart(MPAA ~ ., data=moviesSparse, method = "class")
 prp(movieCart)
@@ -62,7 +79,6 @@ prp(movieCart)
 predictCart = predict(movieCart, newdata = test, type = "class")
 
 table = table(test$MPAA, predictCart)
+for(i in 1:4){if(table[i] == 1)cat(table)}
 print(table)
-
-accuracy = sum(diag(table))/sum(table)
-print(accuracy)
+dt[2,3]
