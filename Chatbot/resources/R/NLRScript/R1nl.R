1236c2fe4d1617ca74a21799ead37a2d162a77d9
Rendall Schijven <rendall.schijven@hotmail.com>
2018-01-25 14:44:01 +0100

    Alle R vragen werken in NL en EN

--- Chatbot/resources/R/NLRScript/R1nl.R
+++ Chatbot/resources/R/NLRScript/R1nl.R
@@ -1,56 +1,88 @@
-# Title     : Culturele films
-# Objective : Ga na welk land meet geweldadige films maakt, frankrijk of de vs
+# Title     : Cultural genre
+# Objective : Check if country1 or country2 have more violence/horror movies return it in dutch
 # Created by: Rendall
-# Created on: 24-1-2018
+# Created on: 15-1-2018
 
 #install.packages("RMySQL")
 #install.packages("ggplot2")
 library(RMySQL)
 library(ggplot2)
 
-mydb <- dbConnect(MySQL(), dbname="NickyBotUtf82", user="riley", password="jayden", host="db.sanderkastelein.nl")
+mydb <- dbConnect(MySQL(), dbname="NickyBotUtf8", user="riley", password="jayden", host="db.sanderkastelein.nl")
+
+#Get the arguments that java sent
+args <- commandArgs(trailingOnly = TRUE)
+
+#Make a string and put all the arguments in iut
+countryString = ""
+for(arg in args){countryString <- paste(countryString, arg)}
+
+#Trim the countries on /. In java we put a / between the country variables
+split = unlist(strsplit(countryString, "/"))
+
+#Save the countries in a variable and trim them so there are no spaces infront or behind
+arg1 = trimws(split[1])
+arg2 = trimws(split[2])
+
+#The queries to get the country from the database
+getCountry1 <- sprintf("SELECT Country FROM Countries WHERE Country LIKE '%%%s%%' LIMIT 1;", arg1)
+getCountry2 <- sprintf("SELECT Country FROM Countries WHERE Country LIKE '%%%s%%' LIMIT 1;", arg2)
+
+#Run the queries and save the results
+country1 <- dbGetQuery(mydb, getCountry1)
+country2 <- dbGetQuery(mydb, getCountry2)
 
 #Get the total amount of movies from each country and the total amount of violent movies
 #We considered the gernes 'Horror', 'Thriller', 'War', and 'Action' as violent
-getFrenchMovieAmount <- ("SELECT COUNT(*) AS movies FROM Movies_Countries AS mc
-                         LEFT JOIN Countries AS c ON mc.Country_ID = c.ID
-                         WHERE c.Country = 'France';")
+getCountry1Movies <- sprintf("SELECT COUNT(*) AS movies FROM Movies_Countries AS mc
+                             LEFT JOIN Countries AS c ON mc.Country_ID = c.ID
+                             WHERE c.Country = '%s';", country1)
 
-getUSAMovieAmount <- ("SELECT COUNT(*) AS Movies FROM Movies_Countries AS mc
-                      LEFT JOIN Countries AS c ON mc.Country_ID = c.ID
-                      WHERE c.Country = 'USA';")
+getCountry2Movies <- sprintf("SELECT COUNT(*) AS Movies FROM Movies_Countries AS mc
+                             LEFT JOIN Countries AS c ON mc.Country_ID = c.ID
+                             WHERE c.Country = '%s';", country2)
 
-getFrenchViolentMovies <- ("SELECT COUNT(*) AS movies FROM Movies_Genres AS mg
-                           LEFT JOIN Genres AS g ON g.Id = mg.Genre_ID
-                           LEFT Join Movies_Countries AS mc ON mc.Movie_ID = mg.Movie_ID
-                           LEFT JOIN Countries AS c ON c.ID = mc.Country_ID
-                           WHERE (g.GenreName = 'Horror' OR g.GenreName = 'Thriller' OR g.GenreName = 'War' OR g.GenreName = 'Action')
-                           AND c.Country = 'France';")
+getCountry1Violent <- sprintf("SELECT COUNT(*) AS movies FROM Movies_Genres AS mg
+                              LEFT JOIN Genres AS g ON g.Id = mg.Genre_ID
+                              LEFT Join Movies_Countries AS mc ON mc.Movie_ID = mg.Movie_ID
+                              LEFT JOIN Countries AS c ON c.ID = mc.Country_ID
+                              WHERE (g.GenreName = 'Horror' OR g.GenreName = 'Thriller' OR g.GenreName = 'War' OR g.GenreName = 'Action')
+                              AND c.Country = '%s';", country1)
 
-getUSAViolentMovies <- ("SELECT COUNT(*) AS movies FROM Movies_Genres AS mg
-                        LEFT JOIN Genres AS g ON g.Id = mg.Genre_ID
-                        LEFT Join Movies_Countries AS mc ON mc.Movie_ID = mg.Movie_ID
-                        LEFT JOIN Countries AS c ON c.ID = mc.Country_ID
-                        WHERE (g.GenreName = 'Horror' OR g.GenreName = 'Thriller' OR g.GenreName = 'War' OR g.GenreName = 'Action')
-                        AND c.Country = 'USA';")
+getCountry2Violent <- sprintf("SELECT COUNT(*) AS movies FROM Movies_Genres AS mg
+                              LEFT JOIN Genres AS g ON g.Id = mg.Genre_ID
+                              LEFT Join Movies_Countries AS mc ON mc.Movie_ID = mg.Movie_ID
+                              LEFT JOIN Countries AS c ON c.ID = mc.Country_ID
+                              WHERE (g.GenreName = 'Horror' OR g.GenreName = 'Thriller' OR g.GenreName = 'War' OR g.GenreName = 'Action')
+                              AND c.Country = '%s';", country2)
 
-frenchMovies <- dbGetQuery(mydb, getFrenchMovieAmount)
-usaMovies <- dbGetQuery(mydb, getUSAMovieAmount)
-frenchViolent <- dbGetQuery(mydb, getFrenchViolentMovies)
-usaViolent <- dbGetQuery(mydb, getUSAViolentMovies)
+Country1Movies <- dbGetQuery(mydb, getCountry1Movies)
+Country2Movies <- dbGetQuery(mydb, getCountry2Movies)
+Country1Violent <- dbGetQuery(mydb, getCountry1Violent)
+Country2Violent <- dbGetQuery(mydb, getCountry2Violent)
 
 #Calculate the percentage of violent movies in eacht of the countries
-francePercentage = (frenchViolent / frenchMovies) * 100
-usaPercentage = (usaViolent / usaMovies) * 100
+c1Percentage = (Country1Violent / Country1Movies) * 100
+c2Percentage = (Country2Violent / Country2Movies) * 100
 
-countries = c("France", "United States")
-violence = c(francePercentage, usaPercentage, recursive = TRUE)
+#Make a dataset of the data we retrieved
+countries = c(toString(country1), toString(country2))
+violence = c(c1Percentage, c2Percentage, recursive = TRUE)
 countriesViolence = data.frame(countries, violence)
 
+#This statement checks which country has the higher percentage and sends the correct string back to the user
+if(c1Percentage > c2Percentage){
+  cat(sprintf("Zoals je ziet heeft %s een hoger percentage geweldadige films dan %s",country1, country2))
+} else{
+  cat(sprintf("Zoals je ziet heeft %s een hoger percentage geweldadige films dan %s",country2, country1))
+}
+
+#Create the barplot and save it as jpg
 invisible(jpeg('violence.jpg'))
-ggplot(countriesViolence, aes(countries, violence)) +
+ggplot(countriesViolence, aes(countries, violence, fill="#FF6666")) +
   geom_bar(stat = "identity", position = "dodge") +
   xlab("Land") +
   ylab("Geweldadige films in %") +
-  ggtitle("Percentage van geweldadige films t.o.v alle films")
+  ggtitle("Geweldadige films als percentage van totale films") +
+  guides(fill=FALSE)
 invisible(dev.off())
--- Chatbot/resources/R/NLRScript/R3nl.R
+++ Chatbot/resources/R/NLRScript/R3nl.R
@@ -5,14 +5,14 @@
 
 library(RMySQL)
 
-mydb <- dbConnect(MySQL(), dbname="NickyBotUtf82", user="riley", password="jayden", host="db.sanderkastelein.nl")
+mydb <- dbConnect(MySQL(), dbname="NickyBotUtf8", user="riley", password="jayden", host="db.sanderkastelein.nl")
 
-query <- ("SELECT Budget, Rating FROM Movies WHERE Budget IS NOT NULL AND Currency = 'EUR' AND Rating IS NOT NULL")
+query <- ("SELECT Budget, Rating FROM Movies WHERE Budget IS NOT NULL AND Currency = 'USD' AND Rating IS NOT NULL ORDER BY RAND() LIMIT 5000")
 
 movies <- dbGetQuery(mydb, query)
 
 invisible(dbDisconnect(mydb))
 
 invisible(jpeg('BudgetRating.jpg'))
-scatter.smooth(movies$Budget / 10000, movies$Duration, main="Verband tussen ", col = "green", lpars =list(col = "red", lwd = 3, lty = 3), xlab = "Budget in ??? x10000", ylab = "Speeltijd")
+scatter.smooth(movies$Budget / 10000, movies$Rating, main="Verband tussen budget en waardering van films", col = "green", lpars =list(col = "red", lwd = 3, lty = 3), xlab = "Budget in ??? x10000", ylab = "Waardering")
 invisible(dev.off())
